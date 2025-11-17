40a64707e26aaefacb3c3e6349abc705a16531cb
Dracosomething <larsfalk08@gmail.com>
2025-11-17 10:26:17 +0100

    Fixed async referencing nonexistend class.

--- src/main/java/io/github/dracosomething/util/Async.java
+++ src/main/java/io/github/dracosomething/util/Async.java
@@ -1,11 +1,10 @@
 package io.github.dracosomething.util;
 
 import java.util.concurrent.*;
-import java.util.Optional;
 import java.lang.Runnable;
 
 public class Async {
   public static void runVoidAsync(Runnable function) throws ExecutionException, InterruptedException {
-    CompleteFuture.runAsync(function);
+    CompletableFuture.runAsync(function);
   }
 }
